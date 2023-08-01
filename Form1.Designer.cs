58ce56fce7b3b6d763b600e58930e68e87114b95
mattosluiseduardo <113482483+mattosluiseduardo@users.noreply.github.com>
2023-07-31 21:47:59 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace protetores
+{
+    partial class Protetores
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.Cpf = new System.Windows.Forms.TextBox();
+            this.textBoxTelefone = new System.Windows.Forms.TextBox();
+            this.textBoxEndereco = new System.Windows.Forms.TextBox();
+            this.textBoxBairro = new System.Windows.Forms.TextBox();
+            this.textBoxCidade = new System.Windows.Forms.TextBox();
+            this.textBoxEstado = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ongs/Protetores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(456, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CNPJ/CPF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(460, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Endereço";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(464, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Bairro";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Cidade";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(464, 281);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Estado";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(40, 353);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Senha";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(32, 105);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(372, 26);
+            this.textBoxNome.TabIndex = 10;
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(460, 103);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(386, 26);
+            this.Email.TabIndex = 11;
+            // 
+            // Cpf
+            // 
+            this.Cpf.Location = new System.Drawing.Point(35, 176);
+            this.Cpf.Name = "Cpf";
+            this.Cpf.Size = new System.Drawing.Size(100, 26);
+            this.Cpf.TabIndex = 12;
+            // 
+            // textBoxTelefone
+            // 
+            this.textBoxTelefone.Location = new System.Drawing.Point(467, 181);
+            this.textBoxTelefone.Name = "textBoxTelefone";
+            this.textBoxTelefone.Size = new System.Drawing.Size(100, 26);
+            this.textBoxTelefone.TabIndex = 13;
+            // 
+            // textBoxEndereco
+            // 
+            this.textBoxEndereco.Location = new System.Drawing.Point(33, 252);
+            this.textBoxEndereco.Name = "textBoxEndereco";
+            this.textBoxEndereco.Size = new System.Drawing.Size(100, 26);
+            this.textBoxEndereco.TabIndex = 14;
+            // 
+            // textBoxBairro
+            // 
+            this.textBoxBairro.Location = new System.Drawing.Point(468, 249);
+            this.textBoxBairro.Name = "textBoxBairro";
+            this.textBoxBairro.Size = new System.Drawing.Size(100, 26);
+            this.textBoxBairro.TabIndex = 15;
+            // 
+            // textBoxCidade
+            // 
+            this.textBoxCidade.Location = new System.Drawing.Point(37, 305);
+            this.textBoxCidade.Name = "textBoxCidade";
+            this.textBoxCidade.Size = new System.Drawing.Size(100, 26);
+            this.textBoxCidade.TabIndex = 16;
+            // 
+            // textBoxEstado
+            // 
+            this.textBoxEstado.Location = new System.Drawing.Point(466, 309);
+            this.textBoxEstado.Name = "textBoxEstado";
+            this.textBoxEstado.Size = new System.Drawing.Size(100, 26);
+            this.textBoxEstado.TabIndex = 17;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(37, 390);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.Size = new System.Drawing.Size(100, 26);
+            this.textBoxSenha.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(475, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 39);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "SALVAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(37, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 39);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Protetores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(963, 551);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.textBoxEstado);
+            this.Controls.Add(this.textBoxCidade);
+            this.Controls.Add(this.textBoxBairro);
+            this.Controls.Add(this.textBoxEndereco);
+            this.Controls.Add(this.textBoxTelefone);
+            this.Controls.Add(this.Cpf);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Protetores";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Protetores_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.TextBox Cpf;
+        private System.Windows.Forms.TextBox textBoxTelefone;
+        private System.Windows.Forms.TextBox textBoxEndereco;
+        private System.Windows.Forms.TextBox textBoxBairro;
+        private System.Windows.Forms.TextBox textBoxCidade;
+        private System.Windows.Forms.TextBox textBoxEstado;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace protetores
+{
+    public partial class Protetores : Form
+    {
+       
+
+        public Protetores()
+        {
+            InitializeComponent();
+        }
+
+        private void Protetores_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost:81, 127.0.0.1;username=root;database=adotapet1;password=");
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO protetores (Nome, Email, Cpf, Telefone, Endereço, Bairro, Cidade, Estado, Senha) VALUES (@Nome, @Email, @Cpf, @Telefone, @Endereço, @Bairro, @Cidade, @Estado, @Senha);", conn);
+            conn.Open();
+            // INSERT (cnpj) VALUES (@CNPJ).
+
+            cmd.Parameters.AddWithValue("@Nome", textBoxNome.Text);
+            if (textBoxNome.Text == "")
+            {
+                MessageBox.Show("Por favor, insira um nome no campo!");
+                textBoxNome.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Email", Email.Text);
+            if (Email.Text == "")
+            {
+                MessageBox.Show("Por favor, insira um Email no campo!");
+                Email.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Cpf", Cpf.Text);
+            if (Cpf.Text == "")
+            {
+                MessageBox.Show("Por favor, insira um Cnpj ou Cpf campo!");
+                Cpf.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Telefone", textBoxTelefone.Text);
+            if (textBoxTelefone.Text == "")
+            {
+                MessageBox.Show("Por favor, insira um Telefone no campo!");
+                textBoxTelefone.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Endereço", textBoxEndereco.Text);
+            if (textBoxEndereco.Text == "")
+            {
+                MessageBox.Show("Por favor, insira um Endereço no campo!");
+                textBoxEndereco.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Bairro", textBoxBairro.Text);
+            if (textBoxBairro.Text == "")
+            { 
+            MessageBox.Show("Por favor, insira um Bairro no campo!");
+            textBoxBairro.Focus();
+            return;
+            }
+
+            cmd.Parameters.AddWithValue("@Cidade", textBoxCidade.Text);
+            if (textBoxCidade.Text == "")
+            {
+                MessageBox.Show("Por favor, insira uma Cidade no campo!");
+                textBoxCidade.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Estado", textBoxEstado.Text);
+            if (textBoxEstado.Text == "")
+            {
+                MessageBox.Show("Por favor, insira um Estado no campo!");
+                textBoxEstado.Focus();
+                return;
+            }
+
+            cmd.Parameters.AddWithValue("@Senha", textBoxSenha.Text);
+            if (textBoxSenha.Text == "")
+            {
+                MessageBox.Show("Por favor, insira uma Senha no campo!");
+                textBoxSenha.Focus();
+                return;
+            }        
+
+
+
+            int rowAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            if (rowAffected > 0)
+            {
+                MessageBox.Show("Dados inseridos com sucesso!");
+                
+            }
+            else
+            {
+                MessageBox.Show("Falha ao inserir dados!");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxNome.Clear();
+            Email.Clear();
+            Cpf.Clear();
+            textBoxTelefone.Clear();
+            textBoxEndereco.Clear();
+            textBoxBairro.Clear();
+            textBoxCidade.Clear();
+            textBoxEstado.Clear();
+            textBoxSenha.Clear();
+
+        }
+    }
+}
+
+
+
+        
+    
+
